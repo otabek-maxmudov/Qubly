bfd9ef001f117d417b207f29aec9e1d0aa27b717
Otabek Makhmudov <72619905+otabek-maxmudov@users.noreply.github.com>
2020-11-19 21:13:31 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// ===== GOOGLE SIGN-IN ===== //
+function onSuccess(googleUser) {
+  console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+}
+function onFailure(error) {
+  console.log(error);
+}
+function renderButton() {
+  gapi.signin2.render('my-signin2', {
+    'scope': 'profile email',
+    'width': 240,
+    'height': 50,
+    'longtitle': true,
+    'theme': 'dark',
+    'onsuccess': onSuccess,
+    'onfailure': onFailure
+  });
+}
+
+// SCROLL 
+
+AOS.init();
+
+AOS.init({
+  disable: 'mobile',
+  duration: 800,
+  once: false,
+  mirror: true,
+})
